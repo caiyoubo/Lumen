b6bc01ba04c62e578180d3805696470fbd6ebdc7
Tony Stark <caiyoubo@aliyun.com>
2018-11-06 11:11:22 +0800

    Study middlerware

---
+++ app/Http/Controllers/Login/LoginController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers\Login;
+
+use App\Http\Controllers\BaseController;
+
+class LoginController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
